386adcb89c4cd5d23fd88684b1c86621f0868924
Matteo <matteovandommelen@outlook.com>
2024-01-16 12:04:00 +0100

    second commit of my python code

--- weather.py
+++ weather.py
@@ -2,14 +2,19 @@ import json
 import csv
 
 weather_info = csv.DictReader(open("stations.csv"))
-
 weather_list = []
-
 for weather in weather_info:
     weather_list.append(weather['Station'])
-
-
-
 Seattle_code = (weather_list[2])
-
 print(Seattle_code)
+
+weather_code = open('precipitation.json')
+weather_precipitation = json.load(weather_code)
+
+total_prep_Seattle = 0
+
+for i in range(len(weather_precipitation)):
+    if weather_precipitation[i]["station"] == Seattle_code:
+        total_prep_Seattle += weather_precipitation[i]["value"]
+    
+print(total_prep_Seattle)
